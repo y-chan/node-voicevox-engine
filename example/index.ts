978276c151669905ff2e2115c77fcff9f3f3383d
y-chan <y-chan@y-chan.dev>
2021-10-12 03:30:52 +0900

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,12 @@
+import * as fs from 'fs'
+
 import Engine from '@/index'
 
 const engine = new Engine('core.dll', false)
-console.log(engine.audio_query('こんにちは、四国めたんです。', 0))
+const speaker_id = 0
+const query = engine.audio_query('こんにちは、四国めたんです。', speaker_id)
+console.log(JSON.stringify(query, null, 2))
+
+const wave_buf = engine.synthesis(query, speaker_id)
+
+fs.writeFileSync('hello.wav', wave_buf)
