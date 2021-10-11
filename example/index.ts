c496d2e91426cddff1f51898d4c774a3d529340c
y-chan <y-chan@y-chan.dev>
2021-10-11 16:39:30 +0900

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,4 @@
 import Engine from '@/index'
 
 const engine = new Engine('core.dll', false)
-console.log(engine.metas())
-console.log(
-  engine.yukarin_s_forward(
-    [
-      0, 14, 14, 8, 21, 21, 36, 21, 21, 12, 21, 21, 21, 21, 14, 15, 40, 22, 21,
-      21, 0,
-    ],
-    1
-  )
-)
+console.log(engine.audio_query('こんにちは、四国めたんです。', 0))
