ee596c9dc7cce5e4553b7f02af00e723da297124
y-chan <y-chan@y-chan.dev>
2022-07-26 14:27:28 +0900

    update example code

--- example/index.ts
+++ example/index.ts
@@ -4,8 +4,16 @@ import Engine from '@/index'
 
 const engine = new Engine('core.dll', false)
 const speaker_id = 0
-const query = engine.audio_query('こんにちは、四国めたんです。', speaker_id)
+const word_uuid = engine.add_user_dict_word(
+  'DeepLearning',
+  'ディープラーニング',
+  4
+)
+console.log(word_uuid)
+console.log(engine.get_user_dict_words())
+const query = engine.audio_query('DeepLearningをしています', speaker_id)
 console.log(JSON.stringify(query, null, 2))
+engine.delete_user_dict_word(word_uuid)
 
 const wave_buf = engine.synthesis(query, speaker_id)
 
